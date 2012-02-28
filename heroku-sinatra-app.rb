1f9253f770f51701bfeb2a5ca03d75811aedd648
Ted Lee <Ted@TedLee.me>
2012-02-28 16:41:50 -0500

    Upload to GH

---
+++ heroku-sinatra-app.rb
@@ -0,0 +1,14 @@
+# You'll need to require these if you
+# want to develop while running with ruby.
+# The config/rackup.ru requires these as well
+# for it's own reasons.
+#
+# $ ruby heroku-sinatra-app.rb
+#
+require 'rubygems'
+require 'sinatra'
+
+configure :production do
+  # Configure stuff here you'll want to
+  # only be run at Heroku at boot
+end
